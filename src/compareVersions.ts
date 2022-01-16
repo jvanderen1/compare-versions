dfe3607330580ba9fbda105a226f2724705ac43f
Joshua Van Deren <livetoscout@gmail.com>
2022-01-16 12:10:57 -0800

    Updated source code

--- src/compareVersions.ts
+++ src/compareVersions.ts
@@ -2,18 +2,24 @@ import { validateAndParse } from './validateAndParse'
 import { split } from './split'
 import { tryParse } from './tryParse'
 
-export const compareVersions = (v1, v2) => {
-  ;[v1, v2].forEach(validateAndParse)
+export const compareVersions = (v1: string, v2: string) => {
+  validateAndParse(v1)
+  validateAndParse(v2)
 
   const s1 = split(v1)
   const s2 = split(v2)
 
   for (let i = 0; i < Math.max(s1.length - 1, s2.length - 1); i += 1) {
-    const n1 = parseInt(s1[i] || 0, 10)
-    const n2 = parseInt(s2[i] || 0, 10)
+    const n1 = parseInt(s1[i] || '0', 10)
+    const n2 = parseInt(s2[i] || '0', 10)
 
-    if (n1 > n2) return 1
-    if (n2 > n1) return -1
+    if (n1 > n2) {
+      return 1
+    }
+
+    if (n2 > n1) {
+      return -1
+    }
   }
 
   const sp1 = s1[s1.length - 1]
@@ -27,16 +33,24 @@ export const compareVersions = (v1, v2) => {
       if (
         p1[i] === undefined ||
         (typeof p2[i] === 'string' && typeof p1[i] === 'number')
-      )
+      ) {
         return -1
+      }
+
       if (
         p2[i] === undefined ||
         (typeof p1[i] === 'string' && typeof p2[i] === 'number')
-      )
+      ) {
         return 1
+      }
 
-      if (p1[i] > p2[i]) return 1
-      if (p2[i] > p1[i]) return -1
+      if (p1[i] > p2[i]) {
+        return 1
+      }
+
+      if (p2[i] > p1[i]) {
+        return -1
+      }
     }
   } else if (sp1 || sp2) {
     return sp1 ? -1 : 1
