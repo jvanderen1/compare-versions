de374f33167efe547601c5acf16967e3debabc28
Joshua Van Deren <livetoscout@gmail.com>
2022-01-16 17:02:32 -0800

    Updated `validate.ts`

--- src/validate.ts
+++ src/validate.ts
@@ -1,5 +1,9 @@
 import { SEMVER } from './constants'
 
-export const validate = (version: string) => {
-  return SEMVER.test(version)
+export const validate = (version: any) => {
+  return (
+    typeof version === 'string' &&
+    /^[v\d]/.test(version) &&
+    SEMVER.test(version)
+  )
 }
